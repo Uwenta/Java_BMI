0d4dfb77d3848d4244f275aa3119e67acf781704
ElenaPonomareva <ponomareva@internet.ru>
2022-05-30 18:58:27 +0500

    reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,5 @@
 public class BmiService {
     public double calculate(double mass, double height) {
-        double bmi = mass / (height*height);
-        return bmi;
+        return mass / (height * height);
     }
 }
